bef4559a099e1f6ef25cd8e7b6010c26ec5afcc2
João Santos <joao.santos@mindswap.academy>
2022-12-20 00:03:46 +0000

    converted images to webp format

--- components/head/index.tsx
+++ components/head/index.tsx
@@ -5,6 +5,7 @@ const MyHead = () => {
     return(
             <Head>
                 <title>ExAds Project</title>
+                <meta name="description" content="Technical challenge"/>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
